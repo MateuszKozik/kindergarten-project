42b64dbf064e06a1db8e744d7e3af4622ca0a2a7
Mateusz Kozik <mateuszkozik100@gmail.com>
2021-10-17 17:41:10 +0200

    Create kindergarten service
    Create service to generate payments and payouts

---
+++ KindergartenService/KindergartenService.cs
@@ -0,0 +1,105 @@
+﻿using KindergartenService.Utility;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+
+namespace KindergartenService
+{
+    public partial class KindergartenService : ServiceBase
+    {
+        Logger logger;
+        static string ApiUrl;
+
+        public KindergartenService()
+        {
+            InitializeComponent();
+            ApiUrl = ConfigurationManager.AppSettings["ApiUrl"];
+            logger = Logger.GetInstance();
+
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            logger.LogMessage("Kindergarten service started!");
+
+            // Set up a timer that triggers every minute.
+            Timer timer = new Timer();
+            timer.Interval = 60000; // 60 seconds
+            timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+
+        }
+
+        public void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            var generatedPayments = ResponseFromKindergartenApi("/Payment/GeneratePayments", "GET");
+            var generatedPayouts = ResponseFromKindergartenApi("/Payout/GeneratePayouts", "GET");
+        }
+
+        protected override void OnStop()
+        {
+            logger.LogMessage("Kindergarten service stopped!");
+        }
+
+        string ResponseFromKindergartenApi(string reference, string methodType, string data = null)
+        {
+            try
+            {
+                var certificateValidation = bool.Parse(ConfigurationManager.AppSettings["ServerCertificateValidation"]);
+
+                var httpRequest = (HttpWebRequest)WebRequest.Create(ApiUrl + reference);
+                httpRequest.Method = methodType; //"POST";
+
+                httpRequest.Accept = "application/json";
+                httpRequest.ContentType = "application/json";
+
+                if (!certificateValidation)
+                {
+                    httpRequest.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+                }
+
+                if (data != null)
+                {
+                    var postData = Encoding.UTF8.GetBytes(data);
+                    httpRequest.ContentLength = postData.Length;
+
+                    using (var stream = httpRequest.GetRequestStream())
+                    {
+                        stream.Write(postData, 0, postData.Length);
+                    }
+                }
+
+
+                var webResponse = httpRequest.GetResponse();
+
+                // Get the stream containing content returned by the server.
+                var dataStream = webResponse.GetResponseStream();
+                // Open the stream using a StreamReader for easy access.
+                var reader = new StreamReader(dataStream);
+                // Read the content.
+                string responseFromServer = reader.ReadToEnd();
+
+                return responseFromServer;
+            }
+            catch (Exception ex)
+            {
+                // check if working
+                logger.LogMessage("Exception: " + ex.Message);
+
+                return null;
+            }
+        }
+
+    }
+}
---
+++ KindergartenService/Utility/Logger.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace KindergartenService.Utility
+{
+    public class Logger
+    {
+        private Logger() { }
+
+        private static Logger _instance;
+
+        private static readonly object _lock = new object();
+
+        public static Logger GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Logger();
+                    }
+                }
+            }
+            return _instance;
+        }
+
+
+        public void LogMessage(string message)
+        {
+            string logFolder = ConfigurationManager.AppSettings["LogPath"];
+            string logFile = ConfigurationManager.AppSettings["LogFile"];
+            string fileName = string.Format("{0}_{1}.log", logFile, DateTime.Now.ToShortDateString());
+
+            if (!Directory.Exists("logFolder"))
+            {
+                Directory.CreateDirectory("logFolder");
+            }
+            string filePath = string.Format(@"{0}\{1}", logFolder, fileName);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(DateTime.Now.ToString());
+            sb.Append(" : ");
+            sb.Append(message);
+            sb.Append('\n');
+
+            StreamWriter writer = new StreamWriter(filePath, true);
+            writer.Write(sb.ToString());
+            writer.Flush();
+            writer.Close();
+        }
+    }
+
+}
