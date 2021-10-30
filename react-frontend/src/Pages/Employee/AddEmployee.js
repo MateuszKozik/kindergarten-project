da0db9c97181b2fbedd31995df3feeea41a5bac0
Mateusz Kozik <mateuszkozik100@gmail.com>
2021-10-30 12:40:44 +0200

    Improve addEmployee view

--- react-frontend/src/Pages/Employee/AddEmployee.js
+++ react-frontend/src/Pages/Employee/AddEmployee.js
@@ -1,45 +1,34 @@
 import React, { Component } from "react";
 import Navbar from "../Navbar";
-import {
-    addEmployee
-} from "../../actions/employeeActions";
-import {
-    registerEmployee
-} from "../../actions/userActions";
-import {
-	getAddresses
-} from "../../actions/addressActions";
-import {
-    getUsers
-} from "../../actions/userActions";
-
+import { addEmployee } from "../../actions/employeeActions";
+import { registerEmployee } from "../../actions/userActions";
+import { getAddresses } from "../../actions/addressActions";
+import { getUsers } from "../../actions/userActions";
 
 import {
 	Grid,
 	Typography,
 	Button,
-    TextField,
-    Select,
-    MenuItem,
-    InputLabel ,
-    FormControl
+	TextField,
+	Select,
+	MenuItem,
+	InputLabel,
+	FormControl
 } from "@material-ui/core";
-
-
 
 class AddEmployee extends Component {
 	state = {
 		name: "",
-        surname: "",
-        phoneNumber: "",
-        position: "",
-        salary: "",
+		surname: "",
+		phoneNumber: "",
+		position: "",
+		salary: "",
 		email: "",
-        password: "",
-        addressId: "",
-        userId: "",
+		password: "",
+		addressId: "",
+		userId: "",
 		address: [],
-        user: []
+		user: []
 	};
 
 	handleChange = (e) => {
@@ -47,48 +36,57 @@ class AddEmployee extends Component {
 	};
 	handleChangeNumber = (e) => {
 		this.setState({ [e.target.name]: parseInt(e.target.value) });
-    };
-
-    componentDidMount() {
-		getAddresses().then((res) => {
-			this.setState({ address: [res.data] });
-        });
-        getUsers().then((res) => {
-			this.setState({ user: [res.data] });
-        });
-    }
-    
-
-    handleAddEmployee = () => {
-		
-		const {email,password} = this.state;
-		const user = {email, password};
-		registerEmployee(user).then((res) =>{
-			this.setState({userId: res.data.id});
-			const {name,surname,phoneNumber,position,salary,addressId,userId} = this.state;
-			const employee = {name,surname,phoneNumber,position,salary,addressId,userId};
-				addEmployee(employee).then((res) => {
-					if(res && res.status === 200){
-						window.location.href = "/employee";
-					}
-				});
-			
-			
-		});
-	
 	};
 
-    render() {
+	componentDidMount() {
+		getAddresses().then((res) => {
+			this.setState({ address: [res.data] });
+		});
+		getUsers().then((res) => {
+			this.setState({ user: [res.data] });
+		});
+	}
+
+	handleAddEmployee = () => {
+		const { email, password } = this.state;
+		const user = { email, password };
+		registerEmployee(user).then((res) => {
+			this.setState({ userId: res.data.id });
+			const {
+				name,
+				surname,
+				phoneNumber,
+				position,
+				salary,
+				addressId,
+				userId
+			} = this.state;
+			const employee = {
+				name,
+				surname,
+				phoneNumber,
+				position,
+				salary,
+				addressId,
+				userId
+			};
+			addEmployee(employee).then((res) => {
+				if (res && res.status === 200) {
+					window.location.href = "/employee";
+				}
+			});
+		});
+	};
+
+	render() {
 		return (
-			<>	
-				<Navbar/>
+			<>
+				<Navbar />
 				<Grid container style={{ textAlign: "center", marginTop: 15 }}>
-                    <Grid item xs={12}>
-						<Typography variant="h5">
-							Dodanie pracownika
-						</Typography>
+					<Grid item xs={12}>
+						<Typography variant="h5">Dodanie pracownika</Typography>
 					</Grid>
-					
+
 					<Grid item xs={12} style={{ marginTop: 15 }}>
 						<Grid container>
 							<Grid item xs={false} md={4} />
@@ -96,7 +94,6 @@ class AddEmployee extends Component {
 								<Grid container spacing={2}>
 									<Grid item xs={12}>
 										<TextField
-											
 											id="name"
 											name="name"
 											onChange={this.handleChange}
@@ -107,7 +104,6 @@ class AddEmployee extends Component {
 									</Grid>
 									<Grid item xs={12}>
 										<TextField
-											
 											id="surname"
 											name="surname"
 											onChange={this.handleChange}
@@ -116,8 +112,8 @@ class AddEmployee extends Component {
 											variant="outlined"
 										/>
 									</Grid>
-                                    <Grid item xs={12}>
-										<TextField	
+									<Grid item xs={12}>
+										<TextField
 											id="phoneNumber"
 											name="phoneNumber"
 											onChange={this.handleChange}
@@ -126,8 +122,8 @@ class AddEmployee extends Component {
 											variant="outlined"
 										/>
 									</Grid>
-                                    <Grid item xs={12}>
-										<TextField	
+									<Grid item xs={12}>
+										<TextField
 											id="position"
 											name="position"
 											onChange={this.handleChange}
@@ -136,67 +132,63 @@ class AddEmployee extends Component {
 											variant="outlined"
 										/>
 									</Grid>
-                                    <Grid item xs={12}>
-										<TextField	
+									<Grid item xs={12}>
+										<TextField
 											id="salary"
 											name="salary"
 											onChange={this.handleChangeNumber}
 											label="Płaca"
-                                            type="number"
+											type="number"
 											required
 											variant="outlined"
 										/>
-									</Grid>  
-                                    <Grid item xs={12} >   
-                                    <FormControl
-                                        required
-                                        variant="outlined"
-                                    >
-                                    <InputLabel id="address-label">Adres</InputLabel>                      
-                                        <Select
-                                        labelId="address-label"
-										id="addressId"
-                                        name="addressId"        
-										value={this.state.addressId}
-                                        onChange={this.handleChange}
-                                        label="Adres"                                    
-                                        style={{ width: 225 }}
-									>
-										
-										{this.state.address[0] && this.state.address[0].map((address) => (
-												<MenuItem
-													key={address.id}
-													value={address.id}
-												>
-													{address.city+","+address.street+","+address.postCode}
-												</MenuItem>
-                                                
-                                        ))}
-									</Select> 
-                                    </FormControl>   
-									</Grid> 
-                                    <Grid item xs={12}>
+									</Grid>
+									<Grid item xs={12}>
+										<FormControl required variant="outlined">
+											<InputLabel id="address-label">Adres</InputLabel>
+											<Select
+												labelId="address-label"
+												id="addressId"
+												name="addressId"
+												value={this.state.addressId}
+												onChange={this.handleChange}
+												label="Adres"
+												style={{ width: 225 }}
+											>
+												{this.state.address[0] &&
+													this.state.address[0].map((address) => (
+														<MenuItem key={address.id} value={address.id}>
+															{address.street +
+																" " +
+																address.houseNumber +
+																", " +
+																address.postCode +
+																" " +
+																address.city}
+														</MenuItem>
+													))}
+											</Select>
+										</FormControl>
+									</Grid>
+									<Grid item xs={12}>
 										<TextField
-											
 											id="email"
 											name="email"
 											onChange={this.handleChange}
 											label="Email"
 											required
-                                            variant="outlined"
-                                            
+											variant="outlined"
 										/>
 									</Grid>
 									<Grid item xs={12}>
 										<TextField
-											
 											id="password"
 											name="password"
 											onChange={this.handleChange}
-                                            label="Hasło"
-                                            required
-                                            variant="outlined"
-                                            type="password"
+											label="Hasło"
+											required
+											variant="outlined"
+											type="password"
 										/>
 									</Grid>
 								</Grid>
@@ -205,20 +197,20 @@ class AddEmployee extends Component {
 						</Grid>
 					</Grid>
 					<Grid item xs={12}></Grid>
-					
+
 					<Grid item xs={12} style={{ marginTop: 20 }}>
-						<Button  variant="contained" color="primary" onClick={this.handleAddEmployee}>
+						<Button
+							variant="contained"
+							color="primary"
+							onClick={this.handleAddEmployee}
+						>
 							Dodaj
 						</Button>
 					</Grid>
-					
-				</Grid>	
+				</Grid>
 			</>
 		);
 	}
 }
-
-
-
 
 export default AddEmployee;
--- react-frontend/src/Pages/Employee/Employee.js
+++ react-frontend/src/Pages/Employee/Employee.js
@@ -1,15 +1,8 @@
 import React, { Component } from "react";
 import Navbar from "../Navbar";
-import {
-	getEmployees,
-    deleteEmployee
-} from "../../actions/employeeActions";
-import {
-	getAddresses
-} from "../../actions/addressActions";
-import {
-	getUsers
-} from "../../actions/userActions";
+import { getEmployees, deleteEmployee } from "../../actions/employeeActions";
+import { getAddresses } from "../../actions/addressActions";
+import { getUsers } from "../../actions/userActions";
 
 import {
 	Grid,
@@ -23,15 +16,11 @@ import {
 	TableRow
 } from "@material-ui/core";
 
-
-
-
 export class Employee extends Component {
 	state = {
 		data: [],
 		address: [],
 		user: []
-
 	};
 
 	componentDidMount() {
@@ -43,169 +32,157 @@ export class Employee extends Component {
 		});
 		getUsers().then((res) => {
 			this.setState({ user: [res.data] });
-			
 		});
-		
-		
-
 	}
 	handleChange = (e) => {
 		this.setState({ [e.target.name]: e.target.value });
 	};
 
-	
-
-
 	render() {
-		
 		return (
 			<>
-				<Navbar/>
+				<Navbar />
 				<Grid
 					container
 					spacing={1}
 					style={{ textAlign: "center", marginTop: 10 }}
 				>
 					<Grid item xs={12}>
-
 						<TableContainer>
 							<Table>
 								<TableHead>
-
 									<TableRow>
-										<TableCell >
-											<Typography >Imię</Typography>
+										<TableCell>
+											<Typography>Imię</Typography>
 										</TableCell>
-										<TableCell >
-											<Typography align="center">Nazwisko</Typography>
-										</TableCell>
-                                       
-										<TableCell >
+										<TableCell>
 											<Typography align="center">Telefon</Typography>
 										</TableCell>
-										<TableCell >
+										<TableCell>
 											<Typography align="center">Stanowisko</Typography>
 										</TableCell>
-                                        <TableCell >
+										<TableCell>
 											<Typography align="center">Płaca</Typography>
 										</TableCell>
-                                        <TableCell >
+										<TableCell>
 											<Typography align="center">Adres</Typography>
 										</TableCell>
-										<TableCell >
+										<TableCell>
 											<Typography align="center">Użytkownik</Typography>
 										</TableCell>
-										<TableCell >
+										<TableCell>
 											<Typography align="center">Akcje</Typography>
 										</TableCell>
 									</TableRow>
 								</TableHead>
 								<TableBody>
-									{this.state.data[0] && this.state.data[0].map((employee) => {
-										
-										return (
-											<TableRow key={employee.id}>
-												<TableCell>
-													<Typography >
-														{employee.name}
-													</Typography>
-												</TableCell>
-                                                <TableCell>
-													<Typography align="center">
-														{employee.surname}
-													</Typography>
-												</TableCell>
-                                                <TableCell>
-													<Typography align="center">
-														{employee.phoneNumber}
-													</Typography>
-												</TableCell>
-												<TableCell>
-													<Typography align="center">
-														{employee.position}
-													</Typography>
-												</TableCell>
-												<TableCell>
-													<Typography align="center">
-														{employee.salary }
-													</Typography>
-												</TableCell>
-                                                <TableCell>
-												{this.state.address[0] && this.state.address[0].map((address) => {
-													return(
-													<Typography align="center">
-														{employee.addressId === address.id ? address.city+","+address.street:''}
-														
-													</Typography>
-													)
-												})}
-												</TableCell>
-												<TableCell>
-												{this.state.user[0] && this.state.user[0].map((user) => {
-													return(
-													<Typography align="center">
-														{employee.userId === user.id ? user.email:''}
-														
-													</Typography>
-													)
-												})}
-												</TableCell>
-													
-												<TableCell align="center">
-                                                <Button style={{ marginLeft: 10 }}
-														variant="contained"
-														color="primary"
-														onClick={() => {
-															this.props.history.push(`/updateEmployee/${employee.id}`)
-														}}
-													>
-														Aktualizuj
-												</Button>
-													<Button style={{ marginLeft: 10 }}
-														variant="contained"
-														color="primary"
-														onClick={() => {
-                                                           
-                                                            deleteEmployee(employee.id).then((res) => {
-                                                                if(res && res.status === 200){
-                                                                   window.location.href = "/employee";
-                                                                }
-                                                            });
-														}}
-													>
-														Usuń
-												</Button>
-                                                
-												</TableCell>
-											</TableRow>
+									{this.state.data[0] &&
+										this.state.data[0].map((employee) => {
+											return (
+												<TableRow key={employee.id}>
+													<TableCell>
+														<Typography>
+															{employee.name} {employee.surname}
+														</Typography>
+													</TableCell>
+													<TableCell>
+														<Typography align="center">
+															{employee.phoneNumber}
+														</Typography>
+													</TableCell>
+													<TableCell>
+														<Typography align="center">
+															{employee.position}
+														</Typography>
+													</TableCell>
+													<TableCell>
+														<Typography align="center">
+															{employee.salary} zł
+														</Typography>
+													</TableCell>
+													<TableCell>
+														{this.state.address[0] &&
+															this.state.address[0].map((address) => {
+																return (
+																	<Typography align="center">
+																		{employee.addressId === address.id
+																			? address.street +
+																			  " " +
+																			  address.houseNumber +
+																			  ", " +
+																			  address.postCode +
+																			  " " +
+																			  address.city
+																			: ""}
+																	</Typography>
+																);
+															})}
+													</TableCell>
+													<TableCell>
+														{this.state.user[0] &&
+															this.state.user[0].map((user) => {
+																return (
+																	<Typography align="center">
+																		{employee.userId === user.id
+																			? user.email
+																			: ""}
+																	</Typography>
+																);
+															})}
+													</TableCell>
 
-										);
-									
-									})}
-
+													<TableCell align="center">
+														<Button
+															style={{ marginLeft: 10 }}
+															variant="contained"
+															color="primary"
+															onClick={() => {
+																this.props.history.push(
+																	`/updateEmployee/${employee.id}`
+																);
+															}}
+														>
+															Aktualizuj
+														</Button>
+														<Button
+															style={{ marginLeft: 10 }}
+															variant="contained"
+															color="primary"
+															onClick={() => {
+																deleteEmployee(employee.id).then((res) => {
+																	if (res && res.status === 200) {
+																		window.location.href = "/employee";
+																	}
+																});
+															}}
+														>
+															Usuń
+														</Button>
+													</TableCell>
+												</TableRow>
+											);
+										})}
 								</TableBody>
 							</Table>
 						</TableContainer>
 					</Grid>
-                    <Grid item xs={12} style={{ textAlign: "left", marginLeft: 10, marginTop: 30 }}>
+					<Grid
+						item
+						xs={12}
+						style={{ textAlign: "left", marginLeft: 10, marginTop: 30 }}
+					>
 						<Button
 							variant="contained"
 							color="primary"
-							onClick={() =>
-								this.props.history.push(`/addEmployee`)
-							}
+							onClick={() => this.props.history.push(`/addEmployee`)}
 						>
 							Dodaj pracownika
 						</Button>
 					</Grid>
-					
 				</Grid>
-
 			</>
 		);
 	}
 }
-
-
 
 export default Employee;
