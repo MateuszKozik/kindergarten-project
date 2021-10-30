e5ff3b2d1f81f1322beb4a527fd5225258735bc7
Mateusz Kozik <mateuszkozik100@gmail.com>
2021-10-30 12:43:59 +0200

    Add getChildrenByParent method to the axios

--- react-frontend/src/actions/childActions.js
+++ react-frontend/src/actions/childActions.js
@@ -2,73 +2,82 @@ import axios from "axios";
 import { hostName } from "./host";
 
 export const getChildes = async () => {
-    try{
-        const res = await axios.get(`${hostName}/Child`);
-        return res;
-        }catch(error){
-            console.log(error);
-        }
-}
+	try {
+		const res = await axios.get(`${hostName}/Child`);
+		return res;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 export const getChild = async (childId) => {
-    try{
-        const res = await axios.get(`${hostName}/Child/${childId}`);
-        return res;
-        }catch(error){
-            console.log(error);
-        }
-}
+	try {
+		const res = await axios.get(`${hostName}/Child/${childId}`);
+		return res;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const getByParent = async (parentId) => {
+	try {
+		const res = await axios.get(`${hostName}/Child/GetByParent/${parentId}`);
+		return res;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 export const addChild = async (child) => {
-    try{
-        const res = await axios.post(`${hostName}/Child/Add`,child);
-        return res;
-        }catch(error){
-            console.log(error);
-        }
-}
+	try {
+		const res = await axios.post(`${hostName}/Child/Add`, child);
+		return res;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
-export const updateChild= async (child) => {
-    try{
-        const res = await axios.post(`${hostName}/Child/Update`,child);
-        return res;
-        }catch(error){
-            console.log(error);
-        }
-}
+export const updateChild = async (child) => {
+	try {
+		const res = await axios.post(`${hostName}/Child/Update`, child);
+		return res;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 export const deleteChild = async (childId) => {
-    try{
-        const res = await axios.get(`${hostName}/Child/Delete/${childId}`);
-        return res;
-        }catch(error){
-            console.log(error);
-        }
-}
+	try {
+		const res = await axios.get(`${hostName}/Child/Delete/${childId}`);
+		return res;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
-export const addByParent = async (id,child) => {
-    try{
-        const res = await axios.post(`${hostName}/Child/AddByParent/${id}`,child);
-        return res;
-        }catch(error){
-            console.log(error);
-        }
-}
+export const addByParent = async (id, child) => {
+	try {
+		const res = await axios.post(`${hostName}/Child/AddByParent/${id}`, child);
+		return res;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 export const getIsSaved = async (childId) => {
-    try{
-        const res = await axios.get(`${hostName}/Child/IsSaved/${childId}`);
-        return res;
-        }catch(error){
-            console.log(error);
-        }
-}
+	try {
+		const res = await axios.get(`${hostName}/Child/IsSaved/${childId}`);
+		return res;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 export const getByGroup = async (groupId) => {
-    try{
-        const res = await axios.get(`${hostName}/child/GetByGroup/${groupId}`);
-        return res;
-        }catch(error){
-            console.log(error);
-        }
-}
+	try {
+		const res = await axios.get(`${hostName}/child/GetByGroup/${groupId}`);
+		return res;
+	} catch (error) {
+		console.log(error);
+	}
+};
